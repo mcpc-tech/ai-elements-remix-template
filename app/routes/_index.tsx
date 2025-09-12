9573b4b3ad58a4f0970a65ea764b384a6ab12d55
yao <63141491+yaonyan@users.noreply.github.com>
2025-09-12 15:18:42 +0800

    feat: add route for Chrome DevTools JSON and update ClientOnly component import

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "react-router";
 import { Navbar } from "components/navbar";
-import { ClientOnly } from "~/components/client-only";
+import { ClientOnly } from "remix-utils/client-only";
 
 import Agent from "~/.client/agent";
 
@@ -18,7 +18,7 @@ export default function Index() {
       <main className="flex-1 flex flex-col">
         <div className="flex-1 container mx-auto max-w-7xl px-6 flex flex-col">
           <ClientOnly fallback={<div>Loading...</div>}>
-            <Agent />
+            {() => <Agent />}
           </ClientOnly>
         </div>
       </main>
