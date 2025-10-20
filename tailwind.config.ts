bb1f40370740c60bba2023ccf7e323764ef47f5c
yao <63141491+yaonyan@users.noreply.github.com>
2025-10-20 18:11:59 +0800

    fix: reorder content paths in Tailwind configuration for proper file inclusion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/.client/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
